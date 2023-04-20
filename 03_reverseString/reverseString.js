40c8d5dd92aa9d62383692727b0ed1c3276694a6
Brandon Kerman <brandonkerman@gmail.com>
2023-04-20 09:48:07 -0700

    Complete exercise '03_reverseString'

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringArray = string.split('') //convert string to array of characters
+    
+    stringArray = stringArray.reverse(); //use reverse() method on array of characters to reverse array
 
+    let newString = stringArray.join(''); //convert array of characters back into a string without the separation of commas
+
+    return newString;
+
+    /*
+    the solution is the above 3 steps^ just simplified on 1 line:
+        return string.split("").reverse().join("");
+    */
 };
 
 // Do not edit below this line
