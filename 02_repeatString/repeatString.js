3d43fe9aa02726901597ff4ce1f00770183da7a1
Brandon Kerman <brandonkerman@gmail.com>
2023-04-20 09:31:50 -0700

    Complete exercise '02_repeatString'

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    //error checking
+    if (num < 0) {
+        return "Error"
+    }
 
+    let newString = ""; //create variable to hold the string we will return
+
+    //use loop to repeat the string the given number ('num') of times
+    for (let i = 0; i < num; i++) {
+        newString = newString + string;
+    }
+
+    return newString; //return variable which now contains the string repeated the given number of times
 };
 
 // Do not edit below this line
