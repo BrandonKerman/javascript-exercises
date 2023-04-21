d18c656fcd5c4ccc3c85d45cdd1d69aad3f41926
Brandon Kerman <brandonkerman@gmail.com>
2023-04-21 09:56:10 -0700

    Complete exercise '07_tempConversion'

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+// Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32
+
+//function to convert Fahrenheit to Celsius
+const convertToCelsius = function(tempFahrenheit) {
+  let tempCelsius = (tempFahrenheit - 32) * (5/9); //conversion formula
+  let tempCelsiusRounded = tempCelsius.toFixed(1); //round to one decimal place
+
+  if (tempCelsiusRounded == 0.0) {
+    return 0;
+  } else return tempCelsiusRounded;
 };
 
-const convertToFahrenheit = function() {
+//function to convert Celsius to Fahrenheit
+const convertToFahrenheit = function(tempCelsius) {
+  let tempFahrenheit = (tempCelsius * (9/5)) + 95; //conversion formula
+  let tempFahrenheitRounded = tempFahrenheit.toFixed(1); //round to one decimal place
+
+  if (tempFahrenheitRounded == 0.0) {
+    return 0;
+  } else return tempFahrenheitRounded;
 };
 
 // Do not edit below this line
